107f717f191e5e7d87166414504f52de65c6a151
BARchitect <barchitect2025@gmail.com>
2026-06-02 19:41:23 -0400

    Added basic move logic for all pieces

--- main.cpp
+++ main.cpp
@@ -140,10 +140,327 @@ struct Board {
                 valid_y.push_back(y);
                 valid_x.push_back(x);
             }
+        } else if (pieces[from_index[0]][from_index[1]] == BISHOP) {
+            bool go_up_left = true, go_up_right = true, go_down_left = true, go_down_right = true;
+            int i = 1;
+
+            while (go_up_left || go_up_right || go_down_left || go_down_right) {
+                color c;
+                if (go_up_left) {
+                    if (c = colors[from_index[0] - i][from_index[1] - i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1] - i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1] - i);
+
+                        go_up_left = false;
+                    } else {
+                        go_up_left = false;
+                    }
+                }
+
+                if (go_up_right) {
+                    if (c = colors[from_index[0] - i][from_index[1] + i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1] + i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1] + i);
+
+                        go_up_right = false;
+                    } else {
+                        go_up_right = false;
+                    }
+                }
+
+                if (go_down_left) {
+                    if (c = colors[from_index[0] + i][from_index[1] - i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1] - i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1] - i);
+
+                        go_down_left = false;
+                    } else {
+                        go_down_left = false;
+                    }
+                }
+
+                if (go_down_right) {
+                    if (c = colors[from_index[0] + i][from_index[1] + i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1] + i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1] + i);
+
+                        go_down_right = false;
+                    } else {
+                        go_down_right = false;
+                    }
+                }
+
+                i++;
+            }
+        } else if (pieces[from_index[0]][from_index[1]] == ROOK) {
+            bool go_left = true, go_right = true, go_up = true, go_down = true;
+            int i = 1;
+
+            while (go_left || go_right || go_up || go_down) {
+                color c;
+
+                if (go_left) {
+                    if (c = colors[from_index[0]][from_index[1] - i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0]);
+                        valid_x.push_back(from_index[1] - i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0]);
+                        valid_x.push_back(from_index[1] - i);
+
+                        go_left = false;
+                    } else {
+                        go_left = false;
+                    }
+                }
+
+                if (go_right) {
+                    if (c = colors[from_index[0]][from_index[1] + i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0]);
+                        valid_x.push_back(from_index[1] + i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0]);
+                        valid_x.push_back(from_index[1] + i);
+
+                        go_right = false;
+                    } else {
+                        go_right = false;
+                    }
+                }
+
+                if (go_up) {
+                    if (c = colors[from_index[0] - i][from_index[1]]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1]);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1]);
+
+                        go_up = false;
+                    } else {
+                        go_up = false;
+                    }
+                }
+
+                if (go_down) {
+                    if (c = colors[from_index[0] + i][from_index[1]]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1]);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1]);
+
+                        go_down = false;
+                    } else {
+                        go_down = false;
+                    }
+                }
+
+                i++;
+            }
+        } else if (pieces[from_index[0]][from_index[1]] == QUEEN) {
+            bool go_left = true, go_right = true, go_up = true, go_down = true,
+                 go_up_left = true, go_up_right = true, go_down_left = true, go_down_right = true;
+            int i = 1;
+
+            while (go_left || go_right || go_up || go_down || go_up_left 
+                   || go_up_right || go_down_left || go_down_right) {
+                color c;
+
+                if (go_left) {
+                    if (c = colors[from_index[0]][from_index[1] - i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0]);
+                        valid_x.push_back(from_index[1] - i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0]);
+                        valid_x.push_back(from_index[1] - i);
+
+                        go_left = false;
+                    } else {
+                        go_left = false;
+                    }
+                }
+
+                if (go_right) {
+                    if (c = colors[from_index[0]][from_index[1] + i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0]);
+                        valid_x.push_back(from_index[1] + i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0]);
+                        valid_x.push_back(from_index[1] + i);
+
+                        go_right = false;
+                    } else {
+                        go_right = false;
+                    }
+                }
+
+                if (go_up) {
+                    if (c = colors[from_index[0] - i][from_index[1]]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1]);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1]);
+
+                        go_up = false;
+                    } else {
+                        go_up = false;
+                    }
+                }
+
+                if (go_down) {
+                    if (c = colors[from_index[0] + i][from_index[1]]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1]);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1]);
+
+                        go_down = false;
+                    } else {
+                        go_down = false;
+                    }
+                }
+
+                if (go_up_left) {
+                    if (c = colors[from_index[0] - i][from_index[1] - i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1] - i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1] - i);
+
+                        go_up_left = false;
+                    } else {
+                        go_up_left = false;
+                    }
+                }
+
+                if (go_up_right) {
+                    if (c = colors[from_index[0] - i][from_index[1] + i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1] + i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] - i);
+                        valid_x.push_back(from_index[1] + i);
+
+                        go_up_right = false;
+                    } else {
+                        go_up_right = false;
+                    }
+                }
+
+                if (go_down_left) {
+                    if (c = colors[from_index[0] + i][from_index[1] - i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1] - i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1] - i);
+
+                        go_down_left = false;
+                    } else {
+                        go_down_left = false;
+                    }
+                }
+
+                if (go_down_right) {
+                    if (c = colors[from_index[0] + i][from_index[1] + i]; c == NOCOLOR) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1] + i);
+                    } else if (c != colors[from_index[0]][from_index[1]]) {
+                        valid_y.push_back(from_index[0] + i);
+                        valid_x.push_back(from_index[1] + i);
+
+                        go_down_right = false;
+                    } else {
+                        go_down_right = false;
+                    }
+                }
+
+                i++;
+            }
+        } else if (pieces[from_index[0]][from_index[1]] == KING) {
+            color c;
+
+            if (c = colors[from_index[0]][from_index[1] - 1]; c == NOCOLOR) {
+                valid_y.push_back(from_index[0]);
+                valid_x.push_back(from_index[1] - 1);
+            } else if (c != colors[from_index[0]][from_index[1]]) {
+                valid_y.push_back(from_index[0]);
+                valid_x.push_back(from_index[1] - 1);
+            }
+            
+            if (c = colors[from_index[0]][from_index[1] + 1]; c == NOCOLOR) {
+                valid_y.push_back(from_index[0]);
+                valid_x.push_back(from_index[1] + 1);
+            } else if (c != colors[from_index[0]][from_index[1]]) {
+                valid_y.push_back(from_index[0]);
+                valid_x.push_back(from_index[1] + 1);
+            }
+        
+            if (c = colors[from_index[0] - 1][from_index[1]]; c == NOCOLOR) {
+                valid_y.push_back(from_index[0] - 1);
+                valid_x.push_back(from_index[1]);
+            } else if (c != colors[from_index[0]][from_index[1]]) {
+                valid_y.push_back(from_index[0] - 1);
+                valid_x.push_back(from_index[1]);
+            }
+        
+            if (c = colors[from_index[0] + 1][from_index[1]]; c == NOCOLOR) {
+                valid_y.push_back(from_index[0] + 1);
+                valid_x.push_back(from_index[1]);
+            } else if (c != colors[from_index[0]][from_index[1]]) {
+                valid_y.push_back(from_index[0] + 1);
+                valid_x.push_back(from_index[1]);
+            }
+        
+            if (c = colors[from_index[0] - 1][from_index[1] - 1]; c == NOCOLOR) {
+                valid_y.push_back(from_index[0] - 1);
+                valid_x.push_back(from_index[1] - 1);
+            } else if (c != colors[from_index[0]][from_index[1]]) {
+                valid_y.push_back(from_index[0] - 1);
+                valid_x.push_back(from_index[1] - 1);
+            }
+            if (c = colors[from_index[0] - 1][from_index[1] + 1]; c == NOCOLOR) {
+                valid_y.push_back(from_index[0] - 1);
+                valid_x.push_back(from_index[1] + 1);
+            } else if (c != colors[from_index[0]][from_index[1]]) {
+                valid_y.push_back(from_index[0] - 1);
+                valid_x.push_back(from_index[1] + 1);
+            }
+        
+            if (c = colors[from_index[0] + 1][from_index[1] - 1]; c == NOCOLOR) {
+                valid_y.push_back(from_index[0] + 1);
+                valid_x.push_back(from_index[1] - 1);
+            } else if (c != colors[from_index[0]][from_index[1]]) {
+                valid_y.push_back(from_index[0] + 1);
+                valid_x.push_back(from_index[1] - 1);
+            }
+            if (c = colors[from_index[0] + 1][from_index[1] + 1]; c == NOCOLOR) {
+                valid_y.push_back(from_index[0] + 1);
+                valid_x.push_back(from_index[1] + 1);
+            } else if (c != colors[from_index[0]][from_index[1]]) {
+                valid_y.push_back(from_index[0] + 1);
+                valid_x.push_back(from_index[1] + 1);
+            }
         }
 
         for (int i = 0; i < valid_y.size(); i++) {
-            if (valid_y[i] == to_index[0] && valid_x[i] == to_index[1]) {
+            if ((valid_y[i] == to_index[0] && valid_x[i] == to_index[1]) &&
+                (valid_y[i] >= 0 && valid_y[i] <= 7 && valid_x[i] >= 0 && valid_x[i] <= 7)) {
                 return true;
             }
         }
@@ -151,7 +468,13 @@ struct Board {
         return false;
     }
 
-    void move_piece(int to_index[2], int from_index[2]) {
+    void move_piece(int to_index[2], int from_index[2], color &turn) {
+        if (colors[from_index[0]][from_index[1]] != turn) {
+            return;
+        } else {
+            turn = (turn == WHITE) ? BLACK : WHITE;
+        }
+
         if (check_piece(to_index, from_index)) {
             pieces[to_index[0]][to_index[1]] = pieces[from_index[0]][from_index[1]];
             pieces[from_index[0]][from_index[1]] = NOPIECE;
@@ -202,6 +525,7 @@ struct Board {
 
 int main() {
     bool running = true;
+    color turn = WHITE;
 
     Board board;
 
@@ -223,7 +547,7 @@ int main() {
 
         cout << "   A  B  C  D  E  F  G  H" << endl << endl;
 
-        cout << " What is your move (\"quit\" to quit, input format is \"A2-C3\")? ";
+        cout << " What is " << ((turn == WHITE)? "white's" : "black's") << " move (\"quit\" to quit, input format is \"A2-C3\")? ";
         cin >> input;
 
         if (input == "quit") {
@@ -248,11 +572,34 @@ int main() {
 
             piece from_piece = board.pieces[from_index[0]][from_index[1]];
 
-            board.move_piece(to_index, from_index);
+            board.move_piece(to_index, from_index, turn);
 
             system("clear");
         }
 
+        bool white_king_found = false, black_king_found = false;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                color piece_color = board.colors[i][j];
+                piece piece_type = board.pieces[i][j];
+
+                if (piece_type == KING) {
+                    if (piece_color == WHITE) {
+                        white_king_found = true;
+                    } else {
+                        black_king_found = true;
+                    }
+                }
+            }
+        }
+
+        if (!white_king_found) {
+            cout << "Black wins!" << endl;
+            running = false;
+        } else if (!black_king_found) {
+            cout << "White wins!" << endl;
+            running = false;
+        }
     }
 
     return 0;
